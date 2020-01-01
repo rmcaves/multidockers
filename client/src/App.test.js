e3142acb3db301c0dde005101f8d88923397bf8f
rmcaves <rmcavestany@gmail.com>
2020-01-01 14:02:19 -0800

    used stephen files

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders learn react link', () => {});
+it('renders without crashing', () => {});
